692d9b3c3af24e49580af58e33baf95def9d8990
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-03-05 14:25:24 -0300

    Add files via upload

--- alarm.py
+++ alarm.py
@@ -1,23 +1,28 @@
 from playsound3 import playsound
-import time
+from time import sleep
+import os
+
+os.system("") #Clears the terminal
 
 #ANSI
-CLEAR = "\033[2J"
-CLEAR_AND_RETURN = "\033[H"
+CLEAR = "\033[2J" #Clears the terminal
+HOME = "\033[H" #Goes back to the top
+CLEAR_LINE = "\033[K" #Erases the rest of the text
 
 def alarm(seconds):
     time_elapsed = 0
 
     print(CLEAR)
     while time_elapsed < seconds:
-        time.sleep(1)
+        sleep(1)
         time_elapsed += 1
 
         time_left = seconds - time_elapsed
         minutes_left = time_left // 60 #Get a integer number; 185 // 60 = 3
         seconds_left = time_left % 60 #Get what couldn't be divided; 185 % 60 = 5
 
-        print(f"{CLEAR_AND_RETURN}Alarm will sound in: {minutes_left:02d}:{seconds_left:02d}")
+        print(f"{HOME}{CLEAR_LINE}Alarm will sound in: {minutes_left:02d}:{seconds_left:02d}")
+    print("Times up!")
     playsound("alarm.mp3")
 
-alarm(10)
+alarm(10) #10 seconds
