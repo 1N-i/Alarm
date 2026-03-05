a4a2d52c358663f2acb29df67d7d614c5ed40581
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-03-05 10:58:27 -0300

    Add files via upload

--- alarm.py
+++ alarm.py
@@ -4,3 +4,16 @@ import time
 
 def alarm(seconds):
     time_elapsed = 0
+
+    while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed += 1
+
+        time_left = seconds - time_elapsed
+        minutes_left = time_left // 60 #Get a integer number; 185 // 60 = 3
+        seconds_left = time_left % 60 #Get what couldn't be divided; 185 % 60 = 5
+
+        print(f"{minutes_left}:{seconds_left}")
+    playsound("alarm.mp3")
+
+alarm(10)
