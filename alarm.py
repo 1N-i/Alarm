fb7b08c4a939a57db48fb9cfec840be4adf9c00f
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-03-05 10:49:45 -0300

    Add files via upload

--- alarm.py
+++ alarm.py
@@ -1,4 +1,6 @@
-print("Hello world!")
+#https://www.fesliyanstudios.com/royalty-free-sound-effects-download/alarm-203
 from playsound3 import playsound
+import time
 
-playsound("alarm.mp3")
+def alarm(seconds):
+    time_elapsed = 0
